858240416dd48dd511ca34ab6eddba3108189db3
dragonline80 <74286955+dragonline80@users.noreply.github.com>
2025-04-16 10:28:04 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const toggleMenu = document.querySelector('.menu');
-const navMenu = document.querySelector('.navbar_menu');
-const navLink = document.querySelector('.navbar_links');
-
-
-toggleMenu.addEventListener('click', () =>{
-    navMenu.classList.toggle('active');
-    navLink.classList.toggle('active');
-})
